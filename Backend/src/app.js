bcf524a39dd8607c2523fc8c960c456959d38924
Aditya <adityaprakash91111@gmail.com>
2026-04-02 20:37:38 +0530

    fix: railway port binding

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,25 +1,21 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const app = express()
 
-const app = express();
 
-const corsOptions = {
+app.use(cors({
   origin: "https://interview-ai-two-teal.vercel.app",
   credentials: true
-};
+}));
 
-app.use(cors(corsOptions));
+app.options("*", cors());
+app.use(express.json())
+app.use(cookieParser())
 
-app.options("/*", cors(corsOptions));
+const authRouter = require("./routes/auth.routes")
+const interviewRouter = require("./routes/interview.routes")
 
-app.use(express.json());
-app.use(cookieParser());
-
-const authRouter = require("./routes/auth.routes");
-const interviewRouter = require("./routes/interview.routes");
-
-app.use("/api/auth", authRouter);
-app.use("/api/interview", interviewRouter);
-
-module.exports = app;
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
+module.exports = app
